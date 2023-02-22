96bbed1c5f3950432f0a68dc87691f479fab9ec6
charlieglen <charlie.penaredondo@gmail.com>
2023-02-22 23:26:02 +1300

    log in test added

---
+++ TestAnalyst2023/TestAnalyst2023/Practice.cs
@@ -0,0 +1,36 @@
+﻿// Open Chrome browser
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+// Launch turnup portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+// Identify the username textbox and enter the valid username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+// Identify the password textbox and enter the valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+
+// Identify login button and click on it
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+
+// Check if user has successfully logged in
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("Log in Successfully");
+}
+else
+{
+    Console.WriteLine("Log In Fail!");
+}
+
+// BRUNO
---
+++ TestAnalyst2023/TestAnalyst2023/Program.cs
@@ -0,0 +1,38 @@
+﻿// Open a browser  - maximise window
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+// Launch the URL
+
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+// Identify the username text box and enter a valid username
+IWebElement usernameBox = driver.FindElement(By.Id("UserName"));
+usernameBox.SendKeys("hari");
+
+
+// Identify the password text box and enter a valid password
+IWebElement passwordBox = driver.FindElement(By.Id("Password"));
+passwordBox.SendKeys("123123");
+
+// Locate log in button and click
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+
+// Check if the log in was successful
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("Log in successful");
+}
+else
+{
+    Console.WriteLine("Log in failed!");
+}
+
+
+//CHARLIE
